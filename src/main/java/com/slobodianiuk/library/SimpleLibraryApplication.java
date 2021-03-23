10f98dcf0fbe45749a213cc2fce0b53ec5921fdf
Igor Dumchykov <Igor_Dumchykov@neimanmarcus.com>
2021-03-23 18:43:24 +0200

    Initial commit

---
+++ src/main/java/com/slobodianiuk/library/SimpleLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.slobodianiuk.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slobodianiuk/library/SimpleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slobodianiuk.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
