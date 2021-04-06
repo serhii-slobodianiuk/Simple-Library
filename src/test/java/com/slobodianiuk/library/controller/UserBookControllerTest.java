f81977279a190dc3ad788d8364f60610964d84e5
serhii-slobodianiuk <sergey.slobodianiuk@gmail.com>
2021-04-06 13:45:44 +0300

    Initial commit

---
+++ src/test/java/com/slobodianiuk/library/controller/UserBookControllerTest.java
@@ -0,0 +1,10 @@
+package com.slobodianiuk.library.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = UserBookController.class)
+public class UserBookControllerTest {
+}
---
+++ src/test/java/com/slobodianiuk/library/dto/UserDtoTest.java
@@ -0,0 +1,8 @@
+package com.slobodianiuk.library.dto;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserDtoTest {
+}
