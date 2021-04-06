74c47285b1088af975343adf395d31709c144fc7
serhii-slobodianiuk <sergey.slobodianiuk@gmail.com>
2021-04-06 12:50:58 +0300

    Made sure the context created controllers

--- src/test/java/com/slobodianiuk/library/SimpleLibraryApplicationTests.java
+++ src/test/java/com/slobodianiuk/library/SimpleLibraryApplicationTests.java
@@ -1,12 +1,30 @@
 package com.slobodianiuk.library;
 
+import com.slobodianiuk.library.controller.BookController;
+import com.slobodianiuk.library.controller.UserBookController;
+import com.slobodianiuk.library.controller.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SimpleLibraryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    UserController userController;
+
+    @Autowired
+    BookController bookController;
+
+    @Autowired
+    UserBookController userBookController;
+
+    @Test
+    void contextLoads() {
+        assertThat(userController).isNotNull();
+        assertThat(bookController).isNotNull();
+        assertThat(userBookController).isNotNull();
+    }
 }
